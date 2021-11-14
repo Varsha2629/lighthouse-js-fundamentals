5e0bdb772bfc187062c7621e881a05e5b998b70f
Varsha Panchal <varsha3051@gmail.com>
2021-11-14 16:34:36 -0500

    hundreds

---
+++ howManyHundreds.js
@@ -0,0 +1,10 @@
+function howManyHundreds(number){
+  let h_number =  Math.floor(number / 100);
+ return h_number;
+}
+
+console.log(howManyHundreds(1000), "=?", 10);
+console.log(howManyHundreds(894), "=?", 8);
+console.log(howManyHundreds(520), "=?", 5);
+console.log(howManyHundreds(99), "=?", 0);
+console.log(howManyHundreds(0), "=?", 0);
