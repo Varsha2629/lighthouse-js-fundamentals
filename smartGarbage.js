9525293386b149151129bea9ef383e18a85e7c0d
Varsha Panchal <varsha3051@gmail.com>
2021-11-15 10:49:18 -0500

    smartGarbage

---
+++ smartGarbage.js
@@ -0,0 +1,13 @@
+const smartGarbage = function (trash, bins) {
+    // Your code in here ...
+    if(trash === 'waste'){
+      bins.waste++;
+    }else if(trash === 'recycling') {
+      bins.recycling++;
+    }else if(trash === 'compost'){
+      bins.compost++;
+    }
+    return bins;
+  
+  }
+  smartGarbage('recycling', { waste: 4, recycling: 2, compost: 5 });
