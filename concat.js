06b6984968c83a5be752dfc4fd8653dbb5305b7b
Varsha Panchal <varsha3051@gmail.com>
2021-11-15 00:36:12 -0500

    marge 2 array

---
+++ concat.js
@@ -0,0 +1,14 @@
+const concat = function(arr1, arr2){
+    let array = [];
+    for(let e of arr1){
+        array.push(e);
+    }   
+    for(let e of arr2){
+        array.push(e);
+    }
+    return array;
+}
+console.log(concat([ 1, 2, 3 ], [ 4, 5, 6 ]), "=?", [ 1, 2, 3, 4, 5, 6 ]);
+console.log(concat([ 0, 3, 1 ], [ 9, 7, 2 ]), "=?", [ 0, 3, 1, 9, 7, 2 ]);
+console.log(concat([], [ 9, 7, 2 ]), "=?", [ 9, 7, 2 ]);
+console.log(concat([ 5, 10 ], []), "=?", [ 5, 10 ]);
